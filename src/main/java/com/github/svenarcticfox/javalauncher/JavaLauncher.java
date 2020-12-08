c9513311780b7787026a6d83ea17eca61075a5f4
SvenArcticFox <tpradez21@gmail.com>
2020-12-08 00:24:35 -0500

    Adds the beginnings of the JavaLauncher class

--- src/main/java/com/github/svenarcticfox/javalauncher/JavaLauncher.java
+++ src/main/java/com/github/svenarcticfox/javalauncher/JavaLauncher.java
@@ -2,6 +2,7 @@ package com.github.svenarcticfox.javalauncher;
 
 import javafx.application.Application;
 import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.control.MenuBar;
@@ -19,6 +20,10 @@ public class JavaLauncher extends Application
     {
         MenuBar menuBar = new MenuBar();
         Menu fileMenu = new Menu("File");
+        MenuItem exitItem = new MenuItem("Exit");
+        fileMenu.getItems().add(exitItem);
+        Menu applicationsMenu = new Menu("Applications");
+
         menuBar.getMenus().add(fileMenu);
         BorderPane borderPane = new BorderPane();
     }
